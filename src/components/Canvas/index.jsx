0c2c6ed7afca12199db9cbaca6fb0944979d4b81
Garrett Head <garretthead@Garretts-MacBook-Pro.local>
2023-09-23 10:04:09 -0500

    StickyNote component

--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -4,6 +4,7 @@ import "./styles.scss";
 
 // COMPONENTS
 import CanvasForm from "../CanvasForm";
+import StickyNote from "../StickyNote";
 
 const initialNoteState = {
   lastNoteCreated: null,
@@ -42,9 +43,8 @@ const Canvas = () => {
   
   const [notesState, dispatch] = useReducer(notesReducer, initialNoteState);
 
-  const addNote = (note) => {
-    dispatch({ type: "ADD_NOTE", payload: note });
-  };
+  const addNote = (note) => { dispatch({ type: "ADD_NOTE", payload: note }) };
+  const deleteNote = (note) => { dispatch({ type: "DELETE_NOTE", payload: note }) };
 
   const dropNote = (event) => {
     event.target.style.top = `${event.pageY - 50}px`;
@@ -62,32 +62,13 @@ const Canvas = () => {
         addNote={addNote}
       />
       {notesState.notes.map((note) => (
-        <div
-          key={note.id}
-          className="note"
-          style={{ transform: `rotate(${note.rotate}deg)` }}
-          draggable="true"
-          onDragEnd={dropNote}
-        >
-          <div
-            className="close-note"
-            onClick={() => dispatch({ type: "DELETE_NOTE", payload: note })}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <pre className="text">{note.text}</pre>
-        </div>
+        <StickyNote
+          key={note.id} 
+          note={note}
+          dropNote={dropNote}
+          deleteNote={deleteNote}
+
+        />
       ))}
     </div>
   );
---
+++ src/components/StickyNote/index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import './styles.scss';
+
+const StickyNote = (props) => {
+  const { note } = props;
+
+  const deleteNote = (note) => {
+    props.deleteNote(note);
+  };
+
+  const dropNote = (event) => {
+    props.dropNote(event);
+  };
+
+  return (
+    <div
+      className="note"
+      style={{ transform: `rotate(${note.rotate}deg)` }}
+      draggable="true"
+      onDragEnd={dropNote}
+    >
+      <div
+        className="close-note"
+        onClick={() => deleteNote(note)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-5 h-5"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+      <pre className="text">{note.text}</pre>
+    </div>
+  );
+};
+
+export default StickyNote;
